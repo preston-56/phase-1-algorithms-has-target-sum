67e39d90d8f11418629ff9056f420e3d0aa29177
Preston <prestonosoro56@gmail.com>
2022-12-09 18:06:59 +0300

    Done

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+   for (let i = 0 ; i < array.length; i++){
+    let element = target - array[i]
+    for (let j = i+1;j<array.length;j++){
+      if (array[j] ===element)return true;
+    }
+  }
+  return false
 }
 
 /* 
